3e53f3165b4fb5ab33a991b84e10f11e673a5a57
YourName <Kkenizez@gmail.com>
2025-03-31 14:47:14 -0400

    New App Friends Giving!

---
+++ FriendsGiving/Guest.swift
@@ -0,0 +1,41 @@
+//
+//  Guest.swift
+//  FriendsGiving
+//
+//  Created by Zhejun Zhang on 3/24/25.
+//
+
+import Foundation
+import SwiftData
+
+@MainActor
+@Model
+class Guest {
+    var name: String
+    var item: String
+    var notes: String
+    
+    init(name: String, item: String, notes: String) {
+        self.name = name
+        self.item = item
+        self.notes = notes
+    }
+    
+    convenience init() {
+        self.init(name: "", item: "", notes: "")
+    }
+}
+
+extension Guest {
+    static var preview: ModelContainer {
+        let container = try! ModelContainer(for: Guest.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+        
+        // Add mock data
+        container.mainContext.insert(Guest(name: "Snoop", item: "Brownies", notes: "A"))
+        container.mainContext.insert(Guest(name: "Travis Kelce", item: "Ribs", notes: "B"))
+        container.mainContext.insert(Guest(name: "Taylor Swift", item: "Cheese Steaks", notes: "C"))
+        container.mainContext.insert(Guest(name: "Beyoncé", item: "Taco Salad", notes: "D"))
+        
+        return container
+    }
+}
